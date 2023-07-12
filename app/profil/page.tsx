b52436f0371636c864bc765503d1aec96313be2f
MarkHenyecz <business@henyeczmark.hu>
2023-07-12 23:57:20 +0100

    profile page setup

--- app/profil/page.tsx
+++ app/profil/page.tsx
@@ -1,3 +1,4 @@
+import AvatarElem from '@/components/avatar/avatar'
 import '../../components/scss/profile.scss'
 import PostElem from '@/components/post/post'
 import Image from 'next/image'
@@ -5,6 +6,11 @@ import Image from 'next/image'
 export default function Profile() {
   return (
     <main>
+      <AvatarElem width={190} height={190} />
+      <h1>flakszxd</h1>
+      <div className='divider' />
+      <h1>Karakterek</h1>
+      
     </main>
   )
 }
